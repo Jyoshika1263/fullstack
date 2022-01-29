ae80eb7fe34cdfa4e5ad1b76c4f8276e82d37e01
Jyoshika Gunduboina <jyoshikagunduboina@gmail.com>
2022-01-29 22:06:35 +0530

    profile

--- jyo.js
+++ jyo.js
@@ -1,8 +1,52 @@
+// let c = document.getElementById("my-canvas");
+// let ctx = c.getContext("2d");
+
+//     let img =  document.createElement("img");
+//     img.onload = () => {
+//         ctx.drawImage(img,0,0,500,500);
+//     };
+// img.src ="/images/idle/1.png";
 let c = document.getElementById("my-canvas");
 let ctx = c.getContext("2d");
 
+let loadImage =(src,callback) => {
     let img =  document.createElement("img");
-    img.onload = () => {
-        ctx.drawImage(img,0,0,500,500);
+    img.onload = () => callback(img);
+    img.src =src;
+};
+
+let imagePath = (frameNumber) => {
+    return "/images/idle/" + frameNumber + ".png";
+};
+
+let loadImages = (callback) => {
+    let images = [];
+    let imagesToLoad = 8;
+
+    [1,2,3,4,5,6,7,8].forEach((frameNumber) => {
+            let path = imagePath(frameNumber);
+
+        loadImage(path, (image) => {
+            images[frameNumber-1] = image;
+            imagesToLoad = imagesToLoad -1;
+
+            if (imagesToLoad === 0) {
+                callback(images);
+            }
+        });
+        });
+};
+ let animate =(ctx,images,animation,callback) => {
+    images.forEach((image,index) => {
+        setTimeout(() => {
+            ctx.clearRect(0,0,500,500);
+            ctx.drawImage(image,0,0,500,500);
+        }, index*100);
+        });
+        setTimeout(callback,images.length*100);
     };
-img.src ="/images/idle/1.png";
+     loadImages((images) => {
+         animate(ctx,images,() => {
+             console.log("done!");
+         });
+     });
